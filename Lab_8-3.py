531103c3f520558726dafdad20425795a19d0a75
P24aelsheikh <144148097+P24aelsheikh@users.noreply.github.com>
2023-12-04 11:56:36 -0500

    Add files via upload

---
+++ Lab_8-3.py
@@ -0,0 +1,23 @@
+"""
+Create a Python file named lab_8-1.py
+
+*** You must write a comment for every chunk of code you write from now on along with your author tag***
+Create a Python file named lab_8-3.py
+Write the while-loop version of the following for-loop program.
+
+def sum_to(n):
+  total = 0
+	for i in range(n+1):
+		total += i
+	return total
+
+The function should be able to have an integer passed to it and return the sum of all the values from 1 to that integer
+"""
+#This is the version with the "while-loop" in it.
+def sum_to(n):
+    total = 0
+    i = 1
+    while i <= n:
+        total += i
+        i += 1
+    return total
